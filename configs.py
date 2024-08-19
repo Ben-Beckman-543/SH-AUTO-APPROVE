7973760e21da06faf4246550ddcd0f788430adf9
sanji-bots-963 <uwvohrc155@nezahuys.xyz>
2024-08-19 22:30:28 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -8,11 +8,11 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "28525384"))
     API_HASH = getenv("API_HASH", "3a1190585fe5bf1f6324be87ba5b68c6")
-    BOT_TOKEN = getenv("BOT_TOKEN", "0")
-    FSUB = getenv("FSUB", "Straw_Hat_Bots")
-    CHID = int(getenv("CHID", "-1002059817947"))
-    SUDO = list(map(int, getenv("SUDO", "6727550037 7162615398").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://sagatobots00001:sagatobots100@cluster00001.vgdshkw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster00001")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7389649893:AAHJnit7KkbHNz8TcxZg76drZeSaT-08DLo")
+    FSUB = getenv("FSUB", "LuffyLeech")
+    CHID = int(getenv("CHID", "-1002180571251"))
+    SUDO = list(map(int, getenv("SUDO", "6727550037 7162615398 7400335416").split()))
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://sagatobots00001:sagatobots100@cluster00001.vgdshkw.mongodb.net/?retryWrites=true&w=majority&appName=REQ_ACCEPTOR_ROBOT")
     
 cfg = Config()
 
